5d183b31ea26102843d0e7f49b27e8b487bd8bf9
ChemsCode <saidiceb@gmail.com>
2024-01-12 22:28:22 -0500

    added persistence

---
+++ ProductService/src/main/java/com/chemseddinesaidi/ProductService/service/ProductService.java
@@ -0,0 +1,7 @@
+package com.chemseddinesaidi.ProductService.service;
+
+import com.chemseddinesaidi.ProductService.model.ProductRequest;
+
+public interface ProductService {
+    long addProduct(ProductRequest productRequest);
+}
---
+++ ProductService/src/main/java/com/chemseddinesaidi/ProductService/service/ProductServiceImpl.java
@@ -0,0 +1,30 @@
+package com.chemseddinesaidi.ProductService.service;
+import com.chemseddinesaidi.ProductService.entity.Product;
+import com.chemseddinesaidi.ProductService.model.ProductRequest;
+import com.chemseddinesaidi.ProductService.repository.ProductRepository;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Log4j2
+public class ProductServiceImpl implements ProductService{
+
+    @Autowired
+    private ProductRepository productRepository;
+    @Override
+    public long addProduct(ProductRequest productRequest) {
+        log.info("addProduct method called with productRequest: {}", productRequest);
+
+        Product product = Product.builder()
+                .productName(productRequest.getName())
+                .quantity(productRequest.getQuantity())
+                .price(productRequest.getPrice())
+                .build();
+
+        productRepository.save(product);
+
+        log.info("Product saved successfully with id: {}", product.getProductId());
+        return product.getProductId();
+    }
+}
