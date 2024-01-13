2ea67ccf2ede3dd5e965ecfa914ec821823852a1
ChemsCode <saidiceb@gmail.com>
2024-01-13 15:22:09 -0500

    added the feign client dependency, allowing the OrderService to call the reduceQuantity in the ProductService API in a clean way

--- OrderService/src/main/java/com/saidichemseddine/OrderService/service/OrderServiceImpl.java
+++ OrderService/src/main/java/com/saidichemseddine/OrderService/service/OrderServiceImpl.java
@@ -1,6 +1,7 @@
 package com.saidichemseddine.OrderService.service;
 
 import com.saidichemseddine.OrderService.entity.OrderEntity;
+import com.saidichemseddine.OrderService.external.client.ProductService;
 import com.saidichemseddine.OrderService.model.OrderRequest;
 import com.saidichemseddine.OrderService.repository.OrderRepository;
 import lombok.extern.log4j.Log4j2;
@@ -16,9 +17,16 @@ public class OrderServiceImpl implements OrderService{
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private ProductService productService;
+
     @Override
     public long placeOrder(OrderRequest orderRequest) {
         log.info("OrderServiceImpl: placeOrder: orderRequest: {}", orderRequest);
+
+        productService.reduceQuantity(orderRequest.getProductId(), orderRequest.getQuantity());
+
+        log.info("Creating Order with Status: CREATED, for the ProductId: {}", orderRequest.getProductId());
         OrderEntity orderEntity = OrderEntity.builder()
                 .productId(orderRequest.getProductId())
                 .quantity(orderRequest.getQuantity())
